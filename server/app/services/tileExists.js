ad66c451e88d649c3ead8a106f5913f1a6177126
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-10 13:16:38 +0200

    step4: 7/9

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,18 @@
-module.exports = null;
+const tileExist = (req, res, next) => {
+    const { coordX, coordY } = req.body;
+    const error = [];
+  if (coordX !== null && coordY !== null) {
+    next();
+  }
+  if(coordX === null){
+    error.push({field: coordX, message: "coordX is wrong"})
+  }
+  if(coordY === null){
+    error.push({field: coordY, message: "coordY is wrong"})
+  }
+  if(error.length !== 0){
+    res.sendStatus(422);
+  }
+};
+
+module.exports = tileExist;
--- server/database/models/TileRepository.js
+++ server/database/models/TileRepository.js
@@ -35,6 +35,17 @@ class TileRepository extends AbstractRepository {
 
     return result;
   }
+
+  async readByCoordinates(coordX, coordY) {
+    if (coordX < 0 || coordX > 11 || coordY < 0 || coordY > 5) {
+      return [];
+    }
+    const query = `SELECT * FROM ${this.table} WHERE coord_x BETWEEN 0 AND 11 AND coord_y BETWEEN 0 AND 5 limit 1`;
+
+    const [rows] = await this.database.query(query);
+
+    return rows;
+  }
 }
 
 module.exports = TileRepository;
