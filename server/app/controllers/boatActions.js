531c853ded0c160301e5e7219b9b522f1af17970
FANNY PRETRE <fanny.pretre@gmail.com>
2024-06-10 11:47:50 +0200

    Step 5 : Done - changed made on 3 and 4 to please ES Lint

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,26 @@ const browse = async (req, res, next) => {
   }
 };
 
+const read = async (req, res, next) => {
+  try {
+    // Rechercher un bateau dans la base de données
+    const boat = await tables.boat.read();
+
+    if (boat) {
+      // Envoyer le bateau trouvé sous forme JSON
+      return res.json(boat);
+    }
+    // Envoyer le statut 204 si aucun bateau n'est trouvé
+    return res.sendStatus(204);
+  } catch (err) {
+    // Gérer les erreurs
+    next(err);
+  }
+
+  return null;
+};
+
 module.exports = {
   browse,
+  read,
 };
