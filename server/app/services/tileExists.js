ff7ce487484731c3f2edc4a2447e0def3535f7f5
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-10 13:27:25 +0200

    step4: 7/9

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,18 +1,11 @@
 const tileExist = (req, res, next) => {
     const { coordX, coordY } = req.body;
-    const error = [];
-  if (coordX !== null && coordY !== null) {
+
+  if (coordX !==null && coordY !== null) {
     next();
-  }
-  if(coordX === null){
-    error.push({field: coordX, message: "coordX is wrong"})
-  }
-  if(coordY === null){
-    error.push({field: coordY, message: "coordY is wrong"})
-  }
-  if(error.length !== 0){
+  } else {
     res.sendStatus(422);
-  }
+  }  
 };
 
 module.exports = tileExist;
