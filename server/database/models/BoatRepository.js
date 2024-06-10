110f5a2faec4740e21e4bd67f3a285688434c86d
Mohamed Jebri <159800102+memedjjebri@users.noreply.github.com>
2024-06-10 15:42:36 +0200

    final commit

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -9,7 +9,7 @@ class BoatRepository extends AbstractRepository {
     const query = `
       SELECT boat.id, boat.coord_x, boat.coord_y, boat.name, tile.type, tile.has_treasure
       FROM boat
-      INNER JOIN tile ON  boat.coord_x=tile.coord_x and boat.coord_y=tile.coord_y
+       JOIN tile ON  boat.coord_x=tile.coord_x and boat.coord_y=tile.coord_y
     `;
     const [rows] = await this.database.query(query);
 
