d855d9e57114906e66bced4ba5e44a90671ede88
FANNY PRETRE <fanny.pretre@gmail.com>
2024-06-10 09:57:19 +0200

    Step 2 : Done

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    // Fetch all tile from the database
+    const tile = await tables.tile.readAll();
+
+    // Respond with the tile in JSON format
+    res.json(tile);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+module.exports = {
+  browse,
+};
