b42b0b7039d596bb32c8516a6cf5913dd51ac055
David L <159317073+daviscrib@users.noreply.github.com>
2024-06-10 12:57:15 +0200

    partial step 3 / 5 / bonus

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -12,6 +12,27 @@ class BoatRepository extends AbstractRepository {
     // Return the array of boats
     return rows;
   }
+
+  async getRandomBoat() {
+    const [rows] = await this.database.query(
+      `select id, name from ${this.table} where type="boat" order by rand() limit 1`
+    );
+
+    return rows[0];
+  }
+
+  async hideTreasure(boat) {
+    const [result] = await this.database.query(
+      `update ${this.table} set has_treasure =
+        case
+          when id = ? then true
+          else false
+        end`,
+      [boat.id]
+    );
+
+    return result;
+  }
 }
 
 module.exports = BoatRepository;
